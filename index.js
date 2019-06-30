ed505ed71608c5ff4a7ec45a75916c9e13c514a3
gopal221296 <gopalakrishnan.v@unacademy.com>
2019-06-30 12:43:25 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ message: "Welcome to Node demo" });
+});
+
+const port = process.env.port || 3000;
+app.listen(port, () => {
+  console.log(`Server started in port ${port}`);
+});
