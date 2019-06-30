b826bbcb1f6f7a8a1053bbdb514c064fb3989991
gopal221296 <gopalakrishnan.v@unacademy.com>
2019-06-30 12:52:14 +0530

    port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
   res.send({ message: "Welcome to Node demo" });
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server started in port ${port}`);
 });
