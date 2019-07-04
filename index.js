bb64d5f6b267a688963a0ea5047275ceb20f3f7c
gopal221296 <gopalakrishnan.v@unacademy.com>
2019-07-04 12:08:53 +0530

    tmdb proxy

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const axios = require("axios");
 const app = express();
 
 app.get("/tmdb", async (req, res) => {
-  const response = await axios.get(
-    "https://api.themoviedb.org/3/trending/movie/week?apikey=628f811dd14b86f8fea17c431c364235"
-  );
-  res.send({ data: response.data });
-  res.send({ message: "Welcome to Node demo 2k19" });
+  try {
+    const response = await axios.get(
+      "https://api.themoviedb.org/3/trending/movie/week?api_key=628f811dd14b86f8fea17c431c364235&language=en-US"
+    );
+    console.log("Response", response);
+    // res.send({ data: response.data });
+    res.send({ data: response.data });
+  } catch (e) {
+    console.log("catch", e);
+    res.send({ message: "Something went wrong" });
+  }
+
+  // res.send({ message: "Welcome to Node demo 2k19" });
 });
 
 const port = process.env.PORT || 3000;
