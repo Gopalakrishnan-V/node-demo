3450b6f3ad32755c36fcaee5c59944f65880629d
gopal221296 <gopalakrishnan.v@unacademy.com>
2019-07-04 11:57:14 +0530

    tmdb

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/tmdb", async (req, res) => {
+  const response = await axios.get(
+    "https://api.themoviedb.org/3/trending/movie/week?apikey=628f811dd14b86f8fea17c431c364235"
+  );
+  res.send({ data: response.data });
   res.send({ message: "Welcome to Node demo 2k19" });
 });
 
